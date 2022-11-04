9445957ffa3557df37a6265c68da6a52e15dd3fc
FerAlvarez33 <fernicomeres@gmail.com>
2022-11-04 12:29:01 -0300

    Ruta edit

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const productsRouter = require('./products')
 const usersRouter = require('./users')
+const cartRouter=require('./cart')
 
 
 
@@ -9,6 +10,8 @@ const usersRouter = require('./users')
 router.use('/products', productsRouter)
 
 router.use('/users', usersRouter)
+
+router.use('/cart', cartRouter)
 
 
 
